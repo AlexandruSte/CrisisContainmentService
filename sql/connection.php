412d32630705336085a462c182876e2c48005ebf
Alex Lungu <lungualex00@gmail.com>
2019-06-10 15:49:40 +0300

    connection to cloud

--- sql/connection.php
+++ sql/connection.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $conn = new PDO("sqlsrv:server = tcp:crisiscontainmentservice.database.windows.net,1433; Database = CrisisContainmentService", "crisisadmin", "...");
+        $conn = new PDO("sqlsrv:server = tcp:crisiscontainmentservice.database.windows.net,1433; Database = CrisisContainmentService", "crisisadmin", "Crisis1234");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = "SELECT * FROM crisis_user";
         foreach ($conn->query($sql) as $row) {
