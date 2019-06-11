e1985285d1ed8a258f01f49a387dd2c6f8513458
Alex Lungu <lungualex00@gmail.com>
2019-06-12 02:41:00 +0300

    fusion fix

--- microservices/fusion/models/AlertFusion.php
+++ microservices/fusion/models/AlertFusion.php
@@ -5,9 +5,11 @@
     class AlertFusion {
 
         private $alert;
-        private $api_key = "...";
-        private $table_id = "...";
-        private $access_token = "...";
+        private $api_key = "AIzaSyC-S2acn9xQZYFSlvdl7xAS8LbPAhUYDCQ";
+        private $past_table_id = "1VK-po2pNqHs7ZUHHBsNH15QsvBTzTdrN6eOS1B3j";
+        private $current_table_id = "1PfXQpnlFSwbcu63QjUMiiwufmTJzZwDzTjjCXXU4";
+        private $access_token = "ya29.GlsmBxMkBJ1B63wJNrM4vJInpMTj0TbDt45SLnCJHwMD1ceEkmoXBTOt5MjSLopjv8V9GKaISlmS-85MY1HBtm4U5OXrPTPABEAZh1kYhTqb7juoigca1U4ymqSk";
+
 
         public function __construct ($alert)
         {
@@ -32,12 +34,16 @@
         public function create()
         {
             $base_url = "https://www.googleapis.com/fusiontables/v2/query";
-            $sql = "INSERT INTO " . $this->table_id . " (Title, Location, Type, Description) VALUES ('"
+            if ($this->alert->getIsSolved() == 1)
+                $table = $this->past_table_id ;
+            else
+                $table = $this->current_table_id;
+            $sql = "INSERT INTO " . $table . " (Id, Title, Location, Type, Description) VALUES ('"
+                .$this->alert->getId()."', '"
                 .$this->alert->getTitle()."', '"
                 .$this->alert->getLatitude(). ", ".$this->alert->getLongitude()."', '"
                 .$this->alert->getType()."', "
                 .($this->alert->getDescription()==null?"NULL":"'".$this->alert->getDescription()."'").")";
-            echo $sql;
             $this->httpPost($base_url, [
                 'sql' => $sql,
                 'key' => $this->api_key
