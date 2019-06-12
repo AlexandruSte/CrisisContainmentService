7adfc14b9b40ca49da9d4a9f099ce27f198ec639
Adrian Gotca <adriangotca98@gmail.com>
2019-06-12 05:13:47 +0300

    made changes in sign-up process

--- views/sign_up.js
+++ views/sign_up.js
@@ -11,3 +11,33 @@ function displayCitizen(){
     authorityForm.style.display="none";
     citizenForm.style.display="block";
 }
+
+function authorityValidate(){
+    if (document.forms["authority"]["name"]=="" ||
+        document.forms["authority"]["email"]=="" ||
+        document.forms["authority"]["password"]=="" ||
+        document.forms["authority"]["confirm_password"]=="" ||
+        document.forms["authority"]["website"]=="" ||
+        document.forms["authority"]["address"]=="" ||
+        document.forms["authority"]["phone"]=="" ||
+        document.forms["authority"]["password"]!=document.forms["authority"]["confirm_password"]){
+        alert("Please complete all fields!");
+        return false;
+    }
+}
+
+function citizenValidate(){
+    if (document.forms["citizen"]["first_name"]=="" ||
+        document.forms["citizen"]["email"]=="" ||
+        document.forms["citizen"]["password"]=="" ||
+        document.forms["citizen"]["confirm_password"]=="" ||
+        document.forms["citizen"]["last_name"]=="" ||
+        document.forms["citizen"]["city"]=="" ||
+        document.forms["citizen"]["country"]=="" ||
+        document.forms["citizen"]["zip"]=="" ||
+        document.forms["citizen"]["phone"]=="" ||
+        document.forms["citizen"]["password"]!=document.forms["citizen"]["confirm_password"]){
+        alert("Please complete all fields!");
+        return false;
+    }
+}
